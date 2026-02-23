939f5e6e4eb12340692306c6bca301b5808a0049
Daniel Aiken <dmaiken13@gmail.com>
2026-02-22 18:59:46 -0700

    Clean up assets and set favicon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Konifer',
   tagline: 'The microservice you need',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
   trailingSlash: false,
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
