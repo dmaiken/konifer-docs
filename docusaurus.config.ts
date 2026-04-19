c0acf33e750aeceac8de8fcc750f8eb0258629b4
Daniel Aiken <dmaiken13@gmail.com>
2026-04-19 15:09:42 -0600

    Fix docusaurus config and upgrade GH actions runners to get off Node 20

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,8 +15,7 @@ const config: Config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://konifer.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
